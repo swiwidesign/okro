9d20d5af1bfc74b3f9ce3407deb6421dc1baad13
swiwidesign <114654312+swiwidesign@users.noreply.github.com>
2026-08-25 12:03:00 +0200

    Update okro_script.js

--- okro_script.js
+++ okro_script.js
@@ -38,7 +38,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     gsap.to(logo, {
         width: "7.75rem",
-
+        marginTop: "0vh",
+        yPercent: 0,
+        bottom: "auto",
         ease: "none",
         scrollTrigger: {
             trigger: ".landing_hero_section",
