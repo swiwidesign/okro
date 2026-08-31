fa71fc701affc7a37073511d4d8fff19d39e5699
swiwidesign <114654312+swiwidesign@users.noreply.github.com>
2026-08-31 10:37:10 +0200

    sd

--- okro_script.js
+++ okro_script.js
@@ -7,55 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
 
 
-    // --------------------------------------------------
-    // LENIS
-    // --------------------------------------------------
-
-    const lenis = new Lenis({
-        autoRaf: false
-    });
-
-    // Keep ScrollTrigger in sync with Lenis
-    lenis.on("scroll", ScrollTrigger.update);
-
-    // Drive Lenis from GSAP's ticker
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 1000);
-    });
-
-    // Prevent GSAP from adding its own lag compensation
-    gsap.ticker.lagSmoothing(0);
-
-
-    // --------------------------------------------------
-    // LENIS CONTROLS
-    // --------------------------------------------------
-
-    document.querySelectorAll("[data-lenis-stop]").forEach((el) => {
-        el.addEventListener("click", () => {
-            lenis.stop();
-        });
-    });
-
-    document.querySelectorAll("[data-lenis-start]").forEach((el) => {
-        el.addEventListener("click", () => {
-            lenis.start();
-        });
-    });
-
-
-    // Lumos nav checkbox
-    const lenisToggle = document.querySelector("[data-lenis-toggle]");
-
-    if (lenisToggle) {
-        lenisToggle.addEventListener("change", () => {
-            if (lenisToggle.checked) {
-                lenis.stop();
-            } else {
-                lenis.start();
-            }
-        });
-    }
 
 
     // --------------------------------------------------
