0945f039d62983a8b3d4e2f9660efc0ed65b5d0b
swiwidesign <114654312+swiwidesign@users.noreply.github.com>
2026-09-15 15:48:56 +0200

    sd

--- okro_script.js
+++ okro_script.js
@@ -194,7 +194,7 @@ window.addEventListener("DOMContentLoaded", () => {
                     scrollTrigger: {
                         trigger: section,
                         containerAnimation: slide,
-                        start: "left center",
+                        start: "left right",
                         end: "right right",
                         scrub: true,
                         invalidateOnRefresh: true
